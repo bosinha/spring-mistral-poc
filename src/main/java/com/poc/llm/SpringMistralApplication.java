b6e61ab30479c5fe731d12fbf90e2d1fa20d31dd
Bonny Sinha <bonnysinha@bonnys-mac-mini.home>
2024-09-15 21:03:56 +0100

    Initial Commit

---
+++ src/main/java/com/poc/llm/SpringMistralApplication.java
@@ -0,0 +1,13 @@
+package com.poc.llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMistralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMistralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/llm/SpringMistralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMistralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
